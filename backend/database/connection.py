b15da46a2a9e419f2ec1e27c93656cc19264fe11
karunaGhimire12 <karunaghimire2007@gmail.com>
2026-06-12 23:42:26 +0545

    feat: update backend setup and configuration

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -2,10 +2,10 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from backend.config import DATABASE_URL
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "sqlite:///./data/students_detail.db"
 
 #Databse connection
 #connect python with database
@@ -13,12 +13,14 @@ engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
+
 #Interaction with databse
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 #It helps to create database table
 Base = declarative_base()
 
